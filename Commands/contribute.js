cf9ff13252677edd72f0ef93e9c4773f434cb30d
dragonexpert <arceus@teamdimensional.net>
2021-09-11 18:11:19 -0500

    Add files via upload

---
+++ Commands/contribute.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "name": "contribute",
+    "description": "Generates a link to the Github Repository",
+    "hasarguments": false,
+    "alias": "github",
+    "execute": (msg, args) =>
+    {
+        return "You can contribute to the Github Repository here: https://github.com/dragonexpert/pokemonmastersbot";
+    }
+}
