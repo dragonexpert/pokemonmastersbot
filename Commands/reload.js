5b84f4154fb7ce6089965aef0b5b9d886ac5a322
dragonexpert <arceus@teamdimensional.net>
2021-10-03 10:02:04 -0500

    Improves reload command
    Makes each cache have its own try / catch block rather than one for the entire thing.  Also states exactly what is reloaded.

--- Commands/reload.js
+++ Commands/reload.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 
 module.exports = {
     "name": "reload",
-    "description": "Reloads the Sync Pair Cache.  Required when edits are made to current sync pairs.",
+    "description": "Reloads the Sync Pair Cache.  Required when edits are made to current sync pairs. If data seems off, try this command.",
     "hasarguments": false,
     "alias": "resync",
-    "auth": 2,
+    "auth": 3,
     "execute": (msg, args) =>
     {
         let syncPairData = fs.readdirSync("SyncPairs").filter(file => file.endsWith(".json"));
@@ -19,30 +19,60 @@ module.exports = {
         {
             for (x in syncPairData)
             {
+
                 delete require.cache[require.resolve("../SyncPairs/" + syncPairData[x])];
                 const syncpair = require("../SyncPairs/" + syncPairData[x]);
                 msg.syncPairs.set(syncpair.name, syncpair);
             }
+            msg.channel.send("Sync pair cache reloaded.");
+        }
+        catch(Exception)
+        {
+            console.error(Exception);
+            msg.channel.send("There was an error reloading the sync pair cache.\n" + Exception);
+        }
+        try
+        {
             for (y in legendPairData)
             {
-                delete require.cache[require.resolve("../LegendaryArena/" + legendPairData[y])];
-                const legendaryPair = require("../LegendaryArena/" + legendPairData[y]);
-                msg.legendaryArena.set(legendaryPair.name, legendaryPair);
+                    delete require.cache[require.resolve("../LegendaryArena/" + legendPairData[y])];
+                    const legendaryPair = require("../LegendaryArena/" + legendPairData[y]);
+                    msg.legendaryArena.set(legendaryPair.name, legendaryPair);
             }
-            for(z in eventData)
+            msg.channel.send("Legendary Arena cache reloaded.");
+        }
+        catch (Exception)
+        {
+            console.error(Exception);
+            msg.channel.send("There was an error reloading the Legendary Arena cache.\n" + Exception);
+        }
+        try
+        {
+            for (z in eventData)
             {
-                delete require.cache[require.resolve("../Events/" + eventData[z])];
-                const event = require("../Events/" + eventData[z]);
-                msg.eventData.set(event.name, event);
+
+                    delete require.cache[require.resolve("../Events/" + eventData[z])];
+                    const event = require("../Events/" + eventData[z]);
+                    msg.eventData.set(event.name, event);
             }
+            msg.channel.send("Event cache reloaded.");
+        }
+        catch (Exception)
+        {
+            console.error(Exception);
+            msg.channel.send("There was an error reloading the Event cache.\n" + Exception);
+        }
+        try
+        {
             delete require.cache[require.resolve("../Passives/passives.json")];
             const passives = require("../Passives/passives.json");
+            msg.channel.send("Passives cache reloaded.");
         }
-        catch (e)
+        catch (Exception)
         {
-            console.error(e);
-            return "There was an error reloading the sync pairs.";
+            console.error(Exception);
+            msg.channel.send("There was an error reloading the passives cache.\n" + Exception);
         }
-        return "The sync pair roster has been updated.";
+        return "Finished reloading all caches.";
     }
 }
