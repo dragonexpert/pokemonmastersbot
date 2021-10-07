df5a392dd2ebc53f00f62c0d5630d27eb46e75a0
dragonexpert <arceus@teamdimensional.net>
2021-10-07 11:31:37 -0500

    Search improvement
    This update makes it so if you forget to put the word type, it will still parse correctly.  It also enables you to do a search of type and one other filter.

--- Commands/search.js
+++ Commands/search.js
@@ -19,8 +19,28 @@ module.exports = {
         }
         // First determine what we are searching for.
 
-        splitter = msg.content.split(" ");
+        let splitter = msg.content.split(" ");
+
+        // DO a sanity check because it is easy to forget to put the word type and just use search water
+        let valid_types = ["normal", "rock", "ground", "steel", "electric", "water", "fire", "grass", "poison", "psychic", "bug", "ice", "flying", "fairy", "fighting", "dark", "ghost"];
+        if(valid_types.includes(splitter[1].toLowerCase()))
+        {
+            let temp_array = [];
+            temp_array[0] = splitter[0];
+            temp_array[1] = "type";
+            temp_array[2] = splitter[1];
+            for(y = 3; y <= splitter.length; y++)
+            {
+                temp_array[y] = splitter[y - 1];
+            }
+            for(z in temp_array)
+            {
+                splitter[z] = temp_array[z];
+            }
+        }
+
         search_criteria = splitter[1].toLowerCase();
+        let length = splitter.length;
         if(search_criteria === "type")
         {
             let comma = "";
@@ -28,7 +48,78 @@ module.exports = {
             let string1 = splitter[2].substring(0, 1).toUpperCase();
             let string2 = splitter[2].substring(1).toLowerCase();
             let type = string1 + string2;
-            let syncpairs = msg.syncPairs.filter(user => user.type === type);
+            let syncpairs = null;
+            if(length === 3)
+            {
+                syncpairs = msg.syncPairs.filter(user => user.type === type);
+            }
+            else
+            {
+                if(splitter[3].toLowerCase() === "striker")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.role.toLowerCase() === "striker" && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "tech")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.role.toLowerCase() === "tech" && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "support")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.role.toLowerCase() === "support" && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "pokefair")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.pokefair === true && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "masterfair" || splitter[3].toLowerCase() === "master")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.masterfair === true && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "ex")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.ex === true && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "mega")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.mega === true && user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "f2p")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.f2p === true && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "seasonal")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.seasonal === true && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "dynamax" || splitter[3].toLowerCase() === "dmax")
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.dynamax === true && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "hp" && !isNaN(splitter[4]))
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.hp >= parseInt(splitter[4]) && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "atk" && !isNaN(splitter[4]))
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.atk >= parseInt(splitter[4]) && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "def" && !isNaN(splitter[4]))
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.def >= parseInt(splitter[4]) && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "spatk" && !isNaN(splitter[4]))
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.spatk >= parseInt(splitter[4]) && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "spdef" && !isNaN(splitter[4]))
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.spdef >= parseInt(splitter[4]) && !user.alias.startsWith("mega"));
+                }
+                if(splitter[3].toLowerCase() === "speed" && !isNaN(splitter[4]))
+                {
+                    syncpairs = msg.syncPairs.filter(user => user.type === type && user.speed >= parseInt(splitter[4]) && !user.alias.startsWith("mega"));
+                }
+            }
             let output = "Search Results: " + syncpairs.size + "\n";
             if(syncpairs.size === 0)
             {
@@ -44,7 +135,7 @@ module.exports = {
         else if(search_criteria === "pokefair")
         {
             let comma = "";
-            let syncpairs = msg.syncPairs.filter(user => user.pokefair === true);
+            let syncpairs = msg.syncPairs.filter(user => user.pokefair === true && !user.alias.startsWith("mega"));
             let output = "Search Results: " + syncpairs.size + "\n";
             if(syncpairs.size === 0)
             {
